206d6f9424ed62e940b902d9a5019538d8bbb457
eman796 <121468747+eman796@users.noreply.github.com>
2023-11-03 21:00:23 -0600

    Commit del 23 de octubre a las 19:11 00:19:44 - Iniciando el Ejercicio 1 Terminé el archivo "01-03 Lectura o entrada de datos.cpp" y

---
+++ 01-IntroduccionYCout&CIN/01-03 Lectura o entrada de datos.cpp
@@ -0,0 +1,14 @@
+//
+// Created by emanu on 24/10/2023.
+//
+//00:14:00 - Leer o escribie datos
+//Para empezar, debemos importar la libreria correspondiente a la entrada y salida de datos, la cual es "iostream"
+#include <iostream>
+using namespace std;// Ponemos eso para evitar poner std::cout o std::cin
+int main(){//Declaramos el Main
+    int number;//Definiendo la variable
+    cout << "Digita un numero "<<endl;//Pedimos al usuario que digite un número
+    cin>>number;// Almacenando el dato del usuario en la variable number
+    cout << "El numero digitado es "+ to_string(number)<<endl;//Se puede hacer así,pero  hay otra forma
+    cout<<"El numero digitado es "<<number;//Y esta es la otra forma
+}
